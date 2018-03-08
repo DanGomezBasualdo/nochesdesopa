afad12974a1996ae3be9453b432cb4b05215c184
Dante Gomez Basualdo <dan.tupar@gmail.com>
2018-03-08 15:42:28 -0300

    Added tables to represent data, added quantity vs maxStock color control, added form to edition

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -15,17 +15,17 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 20, name: 'Enzo', needs: ['jumper XL', 'shirt XL'], location: 'Bolivar 4000' }
     ];
     const items = [
-      { id: 11, name: 'Remera', size: 'M', quantity: 10 },
-      { id: 12, name: 'Remera', size: 'S', quantity: 11 },
-      { id: 13, name: 'Remera', size: 'L', quantity: 5 },
-      { id: 14, name: 'Remera', size: 'XL', quantity: 3 },
-      { id: 15, name: 'Pantalon', size: '32', quantity: 1 },
-      { id: 16, name: 'Pantalon', size: '38', quantity: 1 },
-      { id: 17, name: 'Pantalon', size: '40', quantity: 1 },
-      { id: 18, name: 'Pantalon', size: '46', quantity: 1 },
-      { id: 19, name: 'Pantalon', size: '42', quantity: 1 },
-      { id: 20, name: 'Zapatillas', size: '36', quantity: 1 },
-      { id: 21, name: 'Zapatillas', size: '40', quantity: 1 }
+      { id: 11, name: 'Remera', size: 'M', quantity: 10, maxStock: 20 },
+      { id: 12, name: 'Remera', size: 'S', quantity: 11, maxStock: 20 },
+      { id: 13, name: 'Remera', size: 'L', quantity: 5, maxStock: 20 },
+      { id: 14, name: 'Remera', size: 'XL', quantity: 3, maxStock: 20 },
+      { id: 15, name: 'Pantalon', size: '32', quantity: 1, maxStock: 20 },
+      { id: 16, name: 'Pantalon', size: '38', quantity: 1, maxStock: 20 },
+      { id: 17, name: 'Pantalon', size: '40', quantity: 1, maxStock: 20 },
+      { id: 18, name: 'Pantalon', size: '46', quantity: 1, maxStock: 20 },
+      { id: 19, name: 'Pantalon', size: '42', quantity: 1, maxStock: 20 },
+      { id: 20, name: 'Zapatillas', size: '36', quantity: 1, maxStock: 20 },
+      { id: 21, name: 'Zapatillas', size: '40', quantity: 1, maxStock: 20 },
     ]
     return {heroes, items};
   }
--- src/app/inventory.service.ts
+++ src/app/inventory.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable} from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { Item } from './item';
-import { ITEMS } from './items';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
